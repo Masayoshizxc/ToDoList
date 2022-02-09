fef9ce638970def8235d451fe4d0595a988b83f1
Adilet <masayoshizxc@gmail.com>
2022-02-09 23:59:19 +0600

    Initial Commit

---
+++ againtodo/ToDoDetailTableViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ToDoDetailTableViewController.swift
+//  againtodo
+//
+//  Created by Adilet on 9/2/22.
+//
+
+import UIKit
+
+class ToDoDetailTableViewController: UITableViewController {
+
+    @IBOutlet weak var descriptionField: UITextField!
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var saveBarButton: UIBarButtonItem!
+    
+    var toDoItem : String!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if toDoItem == nil{
+            toDoItem = ""
+        }
+        nameField.text = toDoItem
+    }
+
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        toDoItem = nameField.text
+    }
+    
+    @IBAction func cancelButtonPressed(_ sender: UIBarButtonItem) {
+        let isPresentingAddMode = presentingViewController is UINavigationController
+        if isPresentingAddMode{
+            dismiss(animated: true, completion: nil)
+        }else{
+            navigationController?.popViewController(animated: true)
+        }
+    }
+}
